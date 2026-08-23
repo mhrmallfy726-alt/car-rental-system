7974394f2e5a5d97bff39f55250b619864ad51a4
محمد العلفي <mhrmallfy726@gmail.com>
2026-08-23 17:51:36 +0000

    Improve responsive dashboard layouts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter }from 'react-router-dom'
 import { AuthProvider }from './store/auth'
 import App from './App.jsx'
 import './index.css'
+import './styles/dashboard-responsive.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
