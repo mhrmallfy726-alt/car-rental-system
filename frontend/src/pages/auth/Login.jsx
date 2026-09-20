1210397b5c518e1a536efb09eee477bfc8cd46b2
En_Muharam AL_olofi <mhrmallfy726@gmail.com>
2026-09-20 23:11:38 +0300

    Update branch login verification

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -72,7 +72,7 @@ export default function Login() {
             email,
             verificationToken:
             res.verificationToken,
-            isBranchfirstLogen: true,
+            isBranchFirstLogen: true,
           }
         });
         return;
